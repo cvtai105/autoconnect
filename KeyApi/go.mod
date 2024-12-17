964c7612eec0f051bd0ed5dd86212b487a3f42c8
tai <cvtai105@gmail.com>
2024-12-18 03:28:55 +0700

    add test

---
+++ KeyApi/go.mod
@@ -0,0 +1,3 @@
+module KeyApi
+
+go 1.23.2
--- KeyApi/main.go
+++ KeyApi/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -16,33 +17,41 @@ func keyHandler(w http.ResponseWriter, r *http.Request) {
 	// Lấy giá trị query parameters
 	days := r.URL.Query().Get("days")
 	address := r.URL.Query().Get("address")
+	rawtext := r.URL.Query().Get("rawtext")
 
-	// Kiểm tra xem có thiếu tham số không
-	if days == "" || address == "" {
-		http.Error(w, "Missing query parameters 'days' or 'address'", http.StatusBadRequest)
-		return
+	encrypted := []byte("")
+	result := ""
+	err := error(nil)
+
+	if(rawtext != "") {
+		result = rawtext
+	} else {
+		// Kiểm tra xem có thiếu tham số không
+		if days == "" || address == "" {
+			http.Error(w, "Missing query parameters 'days' or 'address'", http.StatusBadRequest)
+			return
+		}
+
+		// Kiểm tra days có phải là số không
+		daysInt, err := strconv.Atoi(days)
+		if err != nil {
+			http.Error(w, "Invalid value for 'days'", http.StatusBadRequest)
+			return
+		}
+
+		// Tính toán ngày mới (ngày hiện tại + 'days' ngày)
+		currentTime := time.Now()
+		newTime := currentTime.AddDate(0, 0, daysInt)
+
+		// Tạo định dạng thời gian theo yêu cầu
+		formattedDate := newTime.Format("2006-01-02 15:04:05")
+		result = fmt.Sprintf("%s/%s", address, formattedDate)
+
 	}
 
-	// Kiểm tra days có phải là số không
-	daysInt, err := strconv.Atoi(days)
+	encrypted, err = AES_ECB_Encrypt([]byte(result), []byte("c4d7e123f97b8a60"))	
 	if err != nil {
-		http.Error(w, "Invalid value for 'days'", http.StatusBadRequest)
-		return
-	}
-
-	// Tính toán ngày mới (ngày hiện tại + 'days' ngày)
-	currentTime := time.Now()
-	newTime := currentTime.AddDate(0, 0, daysInt)
-
-	// Tạo định dạng thời gian theo yêu cầu
-	formattedDate := newTime.Format("2006-01-02 15:04:05")
-
-	// Tạo chuỗi kết quả theo định dạng yêu cầu
-	result := fmt.Sprintf("%s/%s", address, formattedDate)
-
-	encrypted, err := AES_ECB_Encrypt([]byte(result), []byte("c4d7e123f97b8a60"))	
-	if err != nil {
-		http.Error(w, "Error encrypting result", http.StatusInternalServerError)
+		http.Error(w, "Error encrypting result", http.StatusBadRequest)
 		return
 	}
 
@@ -57,18 +66,54 @@ func keyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprint(result)))
 }
 
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+	// Lấy giá trị query parameters
+	key := r.URL.Query().Get("key")
+
+	//replace " " with "+"
+	key =  strings.Replace(key, " ", "+", -1)
+
+	// Kiểm tra xem có thiếu tham số không
+	if key == "" {
+		http.Error(w, "Missing query parameters 'key'", http.StatusBadRequest)
+		return
+	}
+	
+	keyByte, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		http.Error(w, "Error decrypting result", http.StatusBadRequest)
+		return
+	}
+
+	decrypted, err := AES_ECB_Decrypt(keyByte, []byte("c4d7e123f97b8a60"))
+	if err != nil {
+		http.Error(w, "Error decrypting result", http.StatusBadRequest)
+		return
+	}
+
+	// Gửi kết quả về client
+	w.Header().Set("Content-Type", "application/json")
+	if len(decrypted) == 0 {
+		http.Error(w, "Error decrypting result", http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(decrypted)
+}
+
+
+
 
 func main() {
 	// Định nghĩa route và handler cho /key
-	http.HandleFunc("/key", keyHandler)
+	http.HandleFunc("/encode", keyHandler)
+	http.HandleFunc("/decode", decodeHandler)
 
 	// Bắt đầu server
 	port := "8080"
 	fmt.Printf("Server đang chạy trên cổng %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
-
 
 func AES_ECB_Encrypt(plaintext, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
@@ -85,8 +130,34 @@ func AES_ECB_Encrypt(plaintext, key []byte) ([]byte, error) {
 	for i := 0; i < len(plaintext); i += block.BlockSize() {
 		block.Encrypt(ciphertext[i:i+block.BlockSize()], plaintext[i:i+block.BlockSize()])
 	}
-
 	return ciphertext, nil
+}
+
+
+func AES_ECB_Decrypt(ciphertext, key []byte) ([]byte, error) {
+
+	defer func() {
+		if r := recover(); r != nil {
+		  	fmt.Println("Decryption error: panic")
+		  	return 
+		}
+	}()
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext := make([]byte, len(ciphertext))
+
+	// Decrypt each block
+	for i := 0; i < len(ciphertext); i += block.BlockSize() {
+		block.Decrypt(plaintext[i:i+block.BlockSize()], ciphertext[i:i+block.BlockSize()])
+	}
+
+	// Remove padding
+	padding := int(plaintext[len(plaintext)-1])
+	return plaintext[:len(plaintext)-padding], nil
 }
 
 func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
